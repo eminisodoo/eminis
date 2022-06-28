6ae3bc886084e761757ce617b097889f915627d8
Tony <tony.ballate@gmail.com>
2022-06-28 08:52:00 -0400

    [ADD][EMN-71][gst_eminis_account] - Factura B2C

---
+++ custom/gst_eminis_account/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "GST E-minis Account",
+    "version": "15.0.1",
+    "category": "Invoices & Payments",
+    "author": "Guadaltech Soluciones Tecnológicas S.L.",
+    "website": "http://www.guadaltech.es",
+    "license": "LGPL-3",
+    "description":
+        """
+        Adding new format to Invoice B2C report.
+        """,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "reports/invoice_b2c_report.xml",
+    ]
+}
